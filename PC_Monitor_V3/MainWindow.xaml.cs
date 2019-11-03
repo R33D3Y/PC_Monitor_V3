e4a4f9098c4e714cc2c14b3ac0d03642450ff44d
R33D3Y <reedey2708@gmail.com>
2019-11-03 13:38:32 +0000

    Add project files.

---
+++ PC_Monitor_V3/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using CircularProgressBarApp;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PC_Monitor_V3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double high = 75;
+        private double medium = 50;
+
+        public MainWindow()
+        {
+            ViewModel vm = new ViewModel();
+
+            DataContext = vm;
+            InitializeComponent();
+
+            #region Initialise Components
+
+            vm.ProgressBar = new System.Collections.Generic.Dictionary<string, (CircularProgressBar cpb, TextBlock tb)>();
+            vm.ProgressBar.Add("CPU_LOAD", (CPU_LOAD_CPB, CPU_LOAD_TEXT));
+
+            #endregion
+
+            #region Update Values
+
+            UpdateProgressBar(vm.ProgressBar["CPU_LOAD"], "Load", 75, "%");
+
+            #endregion
+        }
+
+        #region Progress Bar Helpers
+
+        private void UpdateProgressBar((CircularProgressBar cpb, TextBlock tb) tmp, string pre, double value, string unit)
+        {
+            tmp.cpb.Value = value;
+            tmp.tb.Text = pre + "\n" + value.ToString() + unit;
+            tmp.cpb.Background = GetColour(value);
+        }
+
+        private Brush GetColour(double value)
+        {
+            if (value >= high)
+            {
+                return Brushes.Red;
+            }
+
+            else if (value < medium)
+            {
+                return Brushes.Green;
+            }
+
+            else
+            {
+                return Brushes.Orange;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ PC_Monitor_V3/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CircularProgressBarApp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PC_Monitor_V3
+{
+    public class ViewModel : ViewModelBase
+    {
+        private Dictionary<string, (CircularProgressBar cpb, TextBlock tb)> _CircularProgressBar;
+        public Dictionary<string, (CircularProgressBar cpb, TextBlock tb)> ProgressBar
+        {
+            get => _CircularProgressBar;
+            set => SetProperty(ref _CircularProgressBar, value);
+        }
+    }
+
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName]string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
